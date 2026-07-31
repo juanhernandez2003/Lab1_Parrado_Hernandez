a2b763e273399422a340cd7cd9de4ad35f2c8d98
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-07-31 09:07:22 -0500

    feature: Parte 1

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,19 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+        //      Punto 2
+        CountThread t2 = new CountThread(0, 99);
+        CountThread t3 = new CountThread(99, 199);
+        CountThread t4 = new CountThread(200, 299);
+        /*
+        t2.start();
+        t3.start();
+        t4.start();
+        */
+        t2.run();
+        t3.run();
+        t4.run();
+
     }
     
 }
